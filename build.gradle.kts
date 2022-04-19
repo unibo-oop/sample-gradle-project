479c733fe730e9995c5c9f7094b405170713c1ca
Danilo Pianini <danilo.pianini@unibo.it>
2022-04-19 14:53:24 +0200

    build: create a simplified version of the build (deps only, no test)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,6 @@
 plugins {
-    // Apply the java plugin to add support for Java
     java
-
-    // Apply the application plugin to add support for building a CLI application
-    // You can run your app via task "run": ./gradlew run
     application
-
-    /*
-     * Adds tasks to export a runnable jar.
-     * In order to create it, launch the "shadowJar" task.
-     * The runnable jar will be found in build/libs/projectname-all.jar
-     */
-    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
@@ -22,40 +11,10 @@ dependencies {
     // Maven dependencies are composed by a group name, a name and a version, separated by colons
     implementation("com.omertron:API-OMDB:1.5")
     implementation("org.jooq:jool:0.9.14")
-
-    /*
-     * Simple Logging Facade for Java (SLF4J) with Apache Log4j
-     * See: http://www.slf4j.org/
-     */
-    val slf4jVersion = "1.7.36"
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    // Logback backend for SLF4J
+    implementation("org.slf4j:slf4j-api:1.7.36")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.11")
-
-    // JUnit API and testing engine
-    val jUnitVersion = "5.8.2"
-    // when dependencies share the same version, grouping in a val helps to keep them in sync
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
 
 application {
-    // Define the main class for the application.
     mainClass.set("it.unibo.sampleapp.RateAMovie")
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-    }
-
-    withType<Test> {
-        // Enables JUnit 5 Jupiter module
-        useJUnitPlatform()
-    }
 }
