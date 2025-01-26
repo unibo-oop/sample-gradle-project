f33510faf881057a4418ea59744ce76f13b06011
Danilo Pianini <danilo.pianini@unibo.it>
2025-01-26 17:44:56 +0100

    add the foojay resolver convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
+plugins {
+    // Automatically downloads the correct java version to run the static analyzers
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 rootProject.name = "sample-gradle-project"
