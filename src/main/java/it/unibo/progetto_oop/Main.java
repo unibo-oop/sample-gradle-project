645bc2f9764e7d9d53c71879c3d76ee5688cfd5a
Kelly <kelly.applebee@studio.unibo.it>
2025-07-19 15:25:48 +0200

    first main file created for game

---
+++ src/main/java/it/unibo/progetto_oop/Main.java
@@ -0,0 +1,9 @@
+package it.unibo.progetto_oop;
+
+public class Main {
+    public static void main(String[] args) {
+        // CombatModel model = new CombatModel();
+        // CombatView view = new CombatView();
+        // CombatController controller = new CombatController();
+    }
+}
