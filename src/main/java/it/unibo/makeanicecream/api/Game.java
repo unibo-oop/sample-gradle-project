617fed31e7bf230baec6b364bb3e8665a108ca5f
Chiara <chiara.emina@studio.unibo.it>
2026-01-25 17:04:39 +0100

    add method declarations to Game interface

--- src/main/java/it/unibo/makeanicecream/api/Game.java
+++ src/main/java/it/unibo/makeanicecream/api/Game.java
@@ -1,5 +1,14 @@
 package it.unibo.makeanicecream.api;
 
 public interface Game {
-    
+
+    void start(int levelNumber);
+
+    Level getLevel();
+
+    void pause();
+
+    void resume();
+
+    boolean isGameOver();
 }
--- src/main/java/it/unibo/makeanicecream/model/GameImpl.java
+++ src/main/java/it/unibo/makeanicecream/model/GameImpl.java
@@ -1,7 +1,38 @@
 package it.unibo.makeanicecream.model;
 
 import it.unibo.makeanicecream.api.Game;
+import it.unibo.makeanicecream.api.Level;
 
 public class GameImpl implements Game {
+
+    @Override
+    public void start(int levelNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public Level getLevel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLevel'");
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pause'");
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resume'");
+    }
+
+    @Override
+    public boolean isGameOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isGameOver'");
+    }
 
 }
