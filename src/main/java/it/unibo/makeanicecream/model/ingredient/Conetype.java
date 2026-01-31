f32d0149634c015dbc0b1d2fc301d5ee253272c2
Shuyi <shuyi.zhang@studio.unibo.it>
2026-01-31 13:13:37 +0100

    Create folder ingredient and create class enum Conetype

---
+++ src/main/java/it/unibo/makeanicecream/model/ingredient/Conetype.java
@@ -0,0 +1,19 @@
+package it.unibo.makeanicecream.model.ingredient;
+
+/**
+ * Conetype enumeration.
+ */
+public enum Conetype {
+    /**
+     * Classic waffle cone.
+     */
+    CLASSIC,
+    /**
+     * Flower waffle cone.
+     */
+    FLOWER,
+    /**
+     * Cup.
+     */
+    CUP
+}
