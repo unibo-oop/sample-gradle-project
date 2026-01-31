1fe516b596023111e167a9e018f384b27d51f532
Shuyi <shuyi.zhang@studio.unibo.it>
2026-01-31 13:19:38 +0100

    Create class enum for FlavorType

---
+++ src/main/java/it/unibo/makeanicecream/model/ingredient/FlavorType.java
@@ -0,0 +1,13 @@
+package it.unibo.makeanicecream.model.ingredient;
+
+/**
+ * FlavorType enumeration.
+ */
+public enum FlavorType {
+    CHOCOLATE,
+    STRAWBERRY,
+    VANILLA,
+    BULEBERRY,
+    ORANGE,
+    GREENAPPLE
+}
