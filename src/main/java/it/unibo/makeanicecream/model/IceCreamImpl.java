b899e889d05a2bdb36689a4c37281b6c22beec76
Shuyi <shuyi.zhang@studio.unibo.it>
2026-01-31 22:03:28 +0100

    Complete class IceCreamImpl

--- src/main/java/it/unibo/makeanicecream/model/IceCreamImpl.java
+++ src/main/java/it/unibo/makeanicecream/model/IceCreamImpl.java
@@ -1,5 +1,57 @@
 package it.unibo.makeanicecream.model;
 
-public class IceCreamImpl {
+import java.util.List;
+import it.unibo.makeanicecream.api.Icecream;
+import it.unibo.makeanicecream.model.ingredient.Conetype;
+import it.unibo.makeanicecream.model.ingredient.FlavorType;
+import it.unibo.makeanicecream.model.ingredient.LiquidTopping;
+import it.unibo.makeanicecream.model.ingredient.SolidTopping;
 
+public class IceCreamImpl implements Icecream{
+    final Conetype conetype;
+    final List<FlavorType> flavors;
+    final List<LiquidTopping> liquidToppings;
+    final SolidTopping solidTopping;
+
+    /**
+     * Constructor.
+     * @param conetype the type of cone
+     * @param flavors the list of flavors
+     * @param liquidToppings the list of liquid toppings
+     * @param solidTopping the solid topping
+     * @returns new IceCreamImpl instance
+     */
+    public IceCreamImpl(final Conetype conetype, final List<FlavorType> flavors,
+            final List<LiquidTopping> liquidToppings, final SolidTopping solidTopping){
+        
+        this.conetype = conetype;
+        this.flavors = flavors;
+        this.liquidToppings = liquidToppings;
+        this.solidTopping = solidTopping;
+    }
+
+    @Override
+    public Conetype getConetype(){
+        return conetype;
+    }
+
+    @Override
+    public List<FlavorType> getFlavors(){
+        return flavors;
+    }
+
+    @Override
+    public List<LiquidTopping> getLiquidsToppings(){
+        return liquidToppings;
+    }
+
+    @Override
+    public SolidTopping getSolidTopping(){
+        return solidTopping;
+    }
+
+    @Override
+    public boolean isClosed(){
+        return solidTopping != null;
+    }
 }
