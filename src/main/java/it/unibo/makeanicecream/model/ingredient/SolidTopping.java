478b8f0b573a85fc0b835abd0fc1c524b67c6898
Shuyi <shuyi.zhang@studio.unibo.it>
2026-01-31 13:28:50 +0100

    Create enum for SolidTopping

---
+++ src/main/java/it/unibo/makeanicecream/model/ingredient/SolidTopping.java
@@ -0,0 +1,15 @@
+package it.unibo.makeanicecream.model.ingredient;
+
+/**
+ * Topping in solid form enumeration.
+ */
+public enum SolidTopping {
+    /**
+     * Cherry topping.
+     */
+    CHERRY,
+    /**
+     * Cookies topping.
+     */
+    COOKIES
+}
