84d99710bb058db3fc685ae6e9b31269f89d49c7
Shuyi <shuyi.zhang@studio.unibo.it>
2026-02-04 15:59:55 +0100

    Create enum for IngredientType

---
+++ src/main/java/it/unibo/makeanicecream/model/ingredient/IngredientType.java
@@ -0,0 +1,15 @@
+package it.unibo.makeanicecream.model.ingredient;
+
+/**
+ * IngredientType is an enumeration that defines the different types of ingredients that can be used in an ice cream.
+ */
+public enum IngredientType {
+    /**
+     * SCOOP represents a flavor scoop ingredient.
+     * LIQUID_TOPPING represents a liquid topping ingredient.
+     * SOLID_TOPPING represents a solid topping ingredient.
+     */
+    SCOOP, 
+    LIQUID_TOPPING,
+    SOLID_TOPPING,
+}
