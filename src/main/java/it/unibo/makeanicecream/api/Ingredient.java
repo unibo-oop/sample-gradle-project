414ec6165dc245c821b115413e07b6f99107c250
Shuyi <shuyi.zhang@studio.unibo.it>
2026-02-04 16:01:18 +0100

    Create interface Ingredient

---
+++ src/main/java/it/unibo/makeanicecream/api/Ingredient.java
@@ -0,0 +1,8 @@
+package it.unibo.makeanicecream.api;
+
+import it.unibo.makeanicecream.model.ingredient.IngredientType;
+
+public interface Ingredient {
+        /// Get the type of the ingredient.
+        IngredientType getType();
+}
